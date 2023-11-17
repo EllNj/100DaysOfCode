61e7d3b864e6c964bd29ab225cb2aeee0fd54d63
EllNj <ellisnjie@live.co.uk>
2023-11-17 11:16:31 +0000

    day 2/100

---
+++ Day2:100/Day2.01.py
@@ -0,0 +1,6 @@
+age = input()
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+remaining = 90 - int(age)
+remaining *= 52
+print(f"You have {remaining} weeks left.")
---
+++ Day2:100/Day2TipCalc.py
@@ -0,0 +1,7 @@
+print("Welcome to the tip calculator.")
+total = float(input("What was the total bill?"))
+tip = float(input("What percentage tip would you like to give? 10, 12, or 15?"))
+people = float(input("How many people to split the bill?"))
+total = total + ((tip*total) / 100)
+total = round(total / people, 2)
+print(f"Each person should pay: ${total}")
