87de46ceb85a5fbd679cca510479f05f24f02c73
EllNj <ellisnjie@live.co.uk>
2023-11-18 13:56:46 +0000

    day 3/100

---
+++ Day4:100/Day4.01(BankerRoulette).py
@@ -0,0 +1,7 @@
+names = names_string.split(", ")
+# The code above converts the input into an array seperating
+# each name in the input by a comma and space.
+# 🚨 Don't change the code above 👆
+import random
+paying = names[random.randint(0,len(names)-1)]
+print(f"{paying}is going to buy the meal today!")
