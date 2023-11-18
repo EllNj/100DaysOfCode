0ed3bbbcfa321e7087c4e885c40399f754935236
EllNj <ellisnjie@live.co.uk>
2023-11-18 12:43:10 +0000

    day 3/100

--- Day3:100/Day3.06(treasureIsland).py
+++ Day3:100/Day3.06(treasureIsland).py
@@ -1,14 +1,14 @@
 print("Welcome to treasure island.\nYour mission is to find the treasure.")
-first = input("You have came across a split in the road: will you go left or right?(L or R) ")
-if first == "R":
+first = input("You have came across a split in the road: will you go left or right?(L or R) ").lower()
+if first == "r":
     print("You were eaten alive by ravenous koalas.\nGame Over.")
     exit()
-elif first == "L":
-    second = input("You've arrived at a rough river: do you want to swim or wait it out?(S or W) ")
-    if second == "S":
+elif first == "l":
+    second = input("You've arrived at a rough river: do you want to swim or wait it out?(S or W) ").lower()
+    if second == "s":
         print("Ouch! Too many piranhas.\nGame Over.")
         exit()
-    elif second == "W":
+    elif second == "w":
         third = input("after waiting the river calmed and you've now arrived inside the Palace:\nwhich door will you select?(1, 2 or 3) ")
         if third == "1" or third == "3":
             print("The numbers were not in your favour.\nGame Over.")
