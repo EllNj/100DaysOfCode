3209910438fd1f3bd28204cba890391697b3f41a
EllNj <ellisnjie@live.co.uk>
2023-11-22 22:54:37 +0000

    day 7/100

--- Day7:100/Day7.00.py
+++ Day7:100/Day7.00.py
@@ -1,4 +1,5 @@
-#Step 1
+import random
+# Step 1
 
 word_list = ["aardvark", "baboon", "camel"]
 
@@ -7,13 +8,11 @@ word_list = ["aardvark", "baboon", "camel"]
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-import random
 
 chosen_word = random.choice(word_list)
 print(chosen_word)  # this was for testing that it would pick a or b
 guess = input("Guess a letter:\n").lower()
 if guess in list(chosen_word):
-    print ('a')
+    print('a')
 else:
     print('b')
-
