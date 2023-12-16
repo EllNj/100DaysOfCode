b75d4e9b7aab0eef72138b794d6ed7f238cb2799
EllNj <ellisnjie@live.co.uk>
2023-12-16 13:36:37 +0000

    Create Day19_01_turtle_race.py

---
+++ Days11-20/Day19:100/Day19_01_turtle_race.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+leo = Turtle()  # b
+donny = Turtle()    # p
+miccy = Turtle()    # o
+rafa = Turtle()     # r
+
+screen = Screen()
+screen.setup(width=500, height=400)  # this is based on pixels
+
